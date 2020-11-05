80d6c6a87301deaab679f042c25967b32cbd4ba5
mtdunphy-umd <54632032+mtdunphy-umd@users.noreply.github.com>
2020-11-05 18:04:25 -0500

    calories done

--- phone_version/javascript/calories.js
+++ phone_version/javascript/calories.js
@@ -5,3 +5,11 @@ function before_date(){
 function after_date(){
   alert("Would view Nov. 5 info. Not implemented in protoype.");
 }
+
+function save(){
+  alert("Your food is tracked! (Not really in prototype)");
+}
+
+function burned(){
+  alert("Your exercise has been added! (Not really in prototype)");
+}
