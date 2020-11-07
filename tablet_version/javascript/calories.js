513da2f24c5dadd1dcd6825593a7af7b47bcf3b4
mtdunphy-umd <54632032+mtdunphy-umd@users.noreply.github.com>
2020-11-07 12:03:52 -0500

    tablet calories done

---
+++ tablet_version/javascript/calories.js
@@ -0,0 +1,15 @@
+function before_date(){
+  alert("Would view Nov. 1 info. Not implemented in protoype.");
+}
+
+function after_date(){
+  alert("Would view Nov. 5 info. Not implemented in protoype.");
+}
+
+function save(){
+  alert("Your food is tracked! (Not really in prototype)");
+}
+
+function burned(){
+  alert("Your exercise has been added! (Not really in prototype)");
+}
